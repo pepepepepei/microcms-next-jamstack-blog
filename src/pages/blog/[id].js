c4d1f6f7f19964b5de20caf3158e5518e8a1d049
pepepepepei <jumpei.yamada.nagoya@gmail.com>
2023-02-05 20:20:35 +0900

    cssで見た目を装飾した

--- src/pages/blog/[id].js
+++ src/pages/blog/[id].js
@@ -1,14 +1,16 @@
+import styles from "@/styles/Home.module.scss";
 import { client } from "../../../libs/client";
 
 export default function BlogId({ blog }) {
   return (
-    <main>
-      <h1>{blog.title}</h1>
-      <p>{blog.publishedAt}</p>
+    <main className={styles.main}>
+      <h1 className={styles.title}>{blog.title}</h1>
+      <p className={styles.publishedAt}>{blog.publishedAt}</p>
       <div
         dangerouslySetInnerHTML={{
           __html: `${blog.body}`,
         }}
+        className={styles.post}
       />
     </main>
   );
